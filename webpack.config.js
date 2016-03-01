bccbcea4700c31400cce2a6970ed43d5e3fd6ad4
Kyle Coburn <kylecoburn@gmail.com>
2016-03-01 10:22:32 -0800

    Add WebPack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+
+	entry: './public/js/main.js',
+
+	output: {
+		filename: 'bundle.js',
+		path: './public',
+	},
+
+	resolve: {
+		root: [path.resolve('./public'), path.resolve('./public/js')],
+		extensions: ['', '.js', '.css'],
+	},
+
+	module: {
+		loaders: [
+			{ test: /\.css$/, loader: "style-loader!css-loader" },
+			{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=4096' },
+		]
+	},
+
+	externals: {
+		"jquery": "jQuery",
+		"webrtc": "webrtc",
+	},
+
+};
