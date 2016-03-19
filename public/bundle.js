4c5956c1321e4538c5cd24611cf8cb34ab99c15c
Kyle Coburn <kylecoburn@gmail.com>
2016-03-18 17:45:56 -0700

    Alert environments where data cannot be saved

--- public/bundle.js
+++ public/bundle.js
@@ -922,6 +922,30 @@
 
 	var $ = __webpack_require__(14);
 
+	var Config = __webpack_require__(6);
+
+	//SETUP
+
+	var storageAvailable = window.localStorage != null;
+	if (storageAvailable) {
+		try {
+			var availableKey = 'storage_available';
+			window.localStorage.setItem(availableKey, availableKey);
+			window.localStorage.removeItem(availableKey);
+		} catch (e) {
+			storageAvailable = false;
+			console.error(e);
+			if (!Config.TESTING) {
+				window.alert('Local storage unavailable! Your signin information cannot be retrieved, or saved for next time you open the page. You may have private browsing enabled, or set a storage quota that is too small for the site to function.');
+			}
+		}
+	} else {
+		console.error('Local storage undefined');
+		window.alert('Local storage unavailable! Your signin information cannot be retrieved, or saved for next time you open the page. Please make sure you are running the latest version of your browser, or allow local storage if it is disabled.');
+	}
+
+	//PUBLIC
+
 	module.exports = {
 
 		pluralize: function(amount, countable) {
@@ -936,6 +960,9 @@
 		},
 
 		storage: function(key, value) {
+			if (!storageAvailable) {
+				return null; //TODO fallback
+			}
 			if (value === undefined) {
 				return window.localStorage.getItem(key);
 			}
--- public/scripts/util/util.js
+++ public/scripts/util/util.js
@@ -2,6 +2,30 @@
 
 var $ = require('jquery');
 
+var Config = require('util/config');
+
+//SETUP
+
+var storageAvailable = window.localStorage != null;
+if (storageAvailable) {
+	try {
+		var availableKey = 'storage_available';
+		window.localStorage.setItem(availableKey, availableKey);
+		window.localStorage.removeItem(availableKey);
+	} catch (e) {
+		storageAvailable = false;
+		console.error(e);
+		if (!Config.TESTING) {
+			window.alert('Local storage unavailable! Your signin information cannot be retrieved, or saved for next time you open the page. You may have private browsing enabled, or set a storage quota that is too small for the site to function.');
+		}
+	}
+} else {
+	console.error('Local storage undefined');
+	window.alert('Local storage unavailable! Your signin information cannot be retrieved, or saved for next time you open the page. Please make sure you are running the latest version of your browser, or allow local storage if it is disabled.');
+}
+
+//PUBLIC
+
 module.exports = {
 
 	pluralize: function(amount, countable) {
@@ -16,6 +40,9 @@ module.exports = {
 	},
 
 	storage: function(key, value) {
+		if (!storageAvailable) {
+			return null; //TODO fallback
+		}
 		if (value === undefined) {
 			return window.localStorage.getItem(key);
 		}
